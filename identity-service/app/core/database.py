dd297d81fee6246b9b6702c96325f51cdabbba98
Jesse Yang <edujesseyang@gmail.com>
2026-04-16 22:15:55 -0700

    update

--- identity-service/app/core/database.py
+++ identity-service/app/core/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import settings
 
-DATABASE_URL = "postgresql+psycopg://jesse:123456@127.0.0.1:5432/booking_db"
-
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(settings.DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
